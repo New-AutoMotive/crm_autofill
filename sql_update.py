77189101999ed43f9e3f9f36e53fc58dc34cd5ca
bennelmes <ben@newautomotive.org>
2021-09-01 16:04:17 +0100

    first experiment

---
+++ sql_update.py
@@ -0,0 +1,8 @@
+import mysql.connector
+
+cnx = mysql.connector.connect(user='ben', password='new_auto_20',
+                              host='localhost',
+                              database='civicrm_contact')
+
+
+
