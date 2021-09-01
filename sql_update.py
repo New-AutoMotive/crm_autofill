039d9006e0516b63ae5a69aac6e0fa8a43c866d3
ben@newautomotive.org <ben@wordpress-1-vm.europe-west2-a.c.crmserver-id.internal>
2021-09-01 15:11:01 +0000

    create test sql query

--- sql_update.py
+++ sql_update.py
@@ -4,5 +4,17 @@ cnx = mysql.connector.connect(user='ben', password='new_auto_20',
                               host='localhost',
                               database='civicrm_contact')
 
+query = ("SELECT display_name FROM civicrm_contact LIMIT 5")
+
+cursor = cnx.cursor()
+
+cursor.execute(query)
+
+for x in cursor:
+	print(x)
+
+cursor.close()
+cnx.close()
+
 
 
