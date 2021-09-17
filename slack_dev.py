c399f5c4dc68f07756dc06a59be0076beed13f26
ben@newautomotive.org <ben@wordpress-1-vm.europe-west2-a.c.crmserver-id.internal>
2021-09-17 17:01:35 +0000

    anonymising slack key

--- slack_dev.py
+++ slack_dev.py
@@ -1,5 +1,5 @@
 import slack
 from credentials import slack_token
 
-client = slack.WebClient(token='slack_token')
-client.chat_postMessage(channel='#civi-crm', text='Hello! I am the New AutoMotive CiviCRM slack bot... this is just a test - I cannot do much just yet.')
+client = slack.WebClient(token=slack_token)
+client.chat_postMessage(channel='#civi-crm', text='Testing...')
