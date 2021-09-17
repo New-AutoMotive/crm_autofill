191876652b32f5cf4ef5cb026443114263a4ff6d
ben@newautomotive.org <ben@wordpress-1-vm.europe-west2-a.c.crmserver-id.internal>
2021-09-17 09:50:27 +0000

    general update

---
+++ print_disk_status.py
@@ -0,0 +1,27 @@
+import slack
+import os
+os.chdir('/home/ben/crm_autofill/crm_autofill')
+
+dsk = ''
+
+with open('disk_status.txt', 'r') as f:
+    line = f.readlines()
+    dsk = line[3]
+dsk_l = dsk.split(' ')
+dsk_l = [x for x in dsk_l if len(x) > 1]
+
+size = dsk_l[1]
+used = dsk_l[2]
+free = dsk_l[3]
+percent_used = dsk_l[4]
+percent_used_int = int(percent_used[:2])
+
+if percent_used_int < 60:
+    message = "Server disk usage update: the server has a {s} disk, of which {u} is used, and {f} is free. The disk is {p} full. The server has adequate space available.".format(s=size, u=used, f=free, p=percent_used)
+elif (percent_used_int >= 60) & (percent_used_int <80):
+    message = "Server disk usage update: the server has a {s} disk, of which {u} is used, and {f} is free. The disk is {p} full. \n \nAction recommended: increase disk size.".format(s=size, u=used, f=free, p=percent_used)
+elif percent_used_int >= 80:
+    message = "Server disk WARNING: the server has a {s} disk, of which {u} is used, and {f} is free. The disk is {p} full. \n \nACTION REQUIRED: increase disk size soon to prevent server crashing.".format(s=size, u=used, f=free, p=percent_used)
+
+client = slack.WebClient(token='xoxb-1225269354933-2466565277092-8SYhrqoC9BWSdVQcuCgUgU8O')
+client.chat_postMessage(channel = "civi-crm", text = message)
